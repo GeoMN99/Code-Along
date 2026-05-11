fbe723a0200e8a76427158db9972899f6ace9819
George Ngunjiri <ngunjirigeorge995@gmail.com>
2026-05-12 02:48:57 +0300

    feat(address-book.js):script addressbook prototype

--- address-book.js
+++ address-book.js
@@ -4,3 +4,18 @@ function Contact(firstName, lastName, phoneNumber) {
     this.phoneNumber = phoneNumber;
 }
 
+Contact.prototype.fullName =function() {
+    return this.firstName + " " + this.lastName;
+};
+
+function Addressbook() {
+    this.contacts = [];
+    this.currentId = 1;
+}
+
+Addressbook.prototype.addContact =function(contact) {
+    contact.id = this.currentId;
+    this.contacts.push(contact);
+    this.currentId++;
+}
+
