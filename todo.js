ec094167fa932123eeed6f1b0af5186281e1ffe4
George Ngunjiri <ngunjirigeorge995@gmail.com>
2026-05-12 10:22:50 +0300

    feat(todo.js):script addTask & findTask functions

--- todo.js
+++ todo.js
@@ -12,3 +12,22 @@ Task.prototype.markUndone = function() {
     this.done = false;
 };
 
+function TaskList() {
+    this.tasks = [];
+    this.currentId = 1;
+}
+
+TaskList.prototype.addTask = function(task) {
+    task.id = this.currentId;
+    this.currentId++;
+    this.tasks.push(task);
+}
+
+TaskList.prototype.findTask = function(id) {
+    for (let i = 0; i < this.tasks.length; i++) {
+        if (this.tasks[i].id === id) {
+            return this.tasks[i];
+        }
+    }
+    return false;
+};
